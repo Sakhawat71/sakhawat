7857e7f72c5653e30d83f41447d74e75d8cbf65b
sakhawat71 <sakhawat.showrav@gmail.com>
2024-08-23 15:40:56 +0600

    navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,20 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sakhawat Portfolio",
-  description: "Portfolio create by next.js",
+    title: "Sakhawat Portfolio",
+    description: "Front-End Developer with react.js and next.js",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+
+                <div>
+                    {children}
+                </div>
+                
+            </body>
+        </html>
+    );
 }
