d6751ac7d0927c39a32d1505af669f0b0cddacad
sakhawat71 <sakhawat.showrav@gmail.com>
2024-08-25 23:53:01 +0600

    banner part

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Sakhawat || React Dev",
+    title: "Sakhawat | React Dev",
     description: "Front-End Developer with react.js and next.js",
 };
 
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,19 +4,18 @@ import profileImage from '@/assets/logo1.png';
 
 const Banner = () => {
     return (
-        <div className="flex h-screen">
-            {/* Left Section */}
-            <div className="w-1/2 bg-gray-200 flex flex-col justify-center items-start p-16">
+        <div className="flex h-screen mx-auto justify-center">
+
+            <div className="lg:w-1/2 w-full bg-gray-200 flex flex-col justify-center items-start p-16 ">
                 <h1 className="text-4xl font-bold mb-4">Hi, I am</h1>
                 <h2 className="text-5xl font-extrabold text-gray-800 mb-4">Tomasz Gajda</h2>
                 <p className="text-xl text-gray-600">Front-end Developer / UI Designer</p>
-                {/* Add icons or more text here */}
+                
             </div>
 
-            {/* Right Section */}
-            <div className="w-1/2 bg-black flex justify-center items-center">
+            <div className="lg:w-1/2 bg-black hidden lg:flex justify-center items-center">
                 <Image
-                    src={profileImage} // Update this with your image path
+                    src={profileImage}
                     alt="Profile Image"
                     width={400}
                     height={400}
