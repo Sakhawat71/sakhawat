4aa6dad6e56c9b969619c578f26914c201b077e3
sakhawat71 <sakhawat.showrav@gmail.com>
2024-09-05 23:56:28 +0600

    icon on banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,31 @@
 import Image from 'next/image';
 import React from 'react';
 import profileImage from '@/assets/logo1.png';
+import Link from 'next/link';
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Banner = () => {
     return (
         <div className="flex h-screen mx-auto justify-center overflow-hidden font-raleway">
 
-            <div className="lg:w-5/12 w-full bg-[#D7D7D7] flex flex-col justify-center items-start p-16 ">
-                <h1 className="text-4xl font-bold mb-4">Hi, I am</h1>
-                <h2 className="text-5xl font-extrabold text-green-500 mb-4">Sakhawat H</h2>
-                <p className="text-2xl font-extrabold text-[#909090]">Front-end Developer / React Next.js</p>
+            <div className="lg:w-5/12 w-full bg-[#D7D7D7] flex flex-col justify-center items-start p-16 space-y-12">
+                <div>
+                    <h1 className="text-4xl font-bold mb-4">Hi, I am</h1>
+                    <h2 className="text-7xl font-extrabold text-black ">Sakhawat H</h2>
+                    <p className="text-2xl font-extrabold text-[#909090]">Front-end Developer / React Next.js</p>
+                </div>
+
+                <div className='flex space-x-6'>
+                    <Link href="/" className='bg-[#C4C4C4]'>
+                        <FaTwitter className='text-2xl w-9 h-9' />
+                    </Link>
+                    <Link href="/" className='bg-[#C4C4C4]'>
+                        <FaGithub className='text-2xl w-9 h-9' />
+                    </Link>
+                    <Link href="/" className='bg-[#C4C4C4]'>
+                        <FaLinkedin className='text-2xl w-9 h-9' />
+                    </Link>
+                </div>
 
             </div>
 
