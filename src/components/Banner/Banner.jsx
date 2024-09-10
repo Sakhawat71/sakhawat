1df8d032f67d667d0cbcab47d96077f3450aa67b
sakhawat71 <sakhawat.showrav@gmail.com>
2024-09-10 06:19:07 +0600

    responsive banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,16 +6,16 @@ import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Banner = () => {
     return (
-        <div className="flex h-screen mx-auto justify-center overflow-hidden font-raleway">
+        <div className="flex h-screen mx-auto justify-center overflow-hidden font-raleway lg:max-h-[90vh]">
 
-            <div className="lg:w-5/12 w-full bg-[#D7D7D7] flex flex-col justify-center items-start p-16 space-y-12">
+            <div className="lg:w-5/12 w-full bg-[#D7D7D7] flex flex-col  justify-center lg:items-start p-10 md:p-16 space-y-12 lg:pl-28 md:items-center md:text-center lg:text-start ">
                 <div>
-                    <h1 className="text-4xl font-bold mb-4">Hi, I am</h1>
-                    <h2 className="text-6xl font-extrabold text-black ">Sakhawat H</h2>
-                    <p className="text-2xl font-extrabold text-[#909090]">Front-end Developer / React Next.js</p>
+                    <h1 className="md:text-4xl font-bold mb-4">Hi, I am</h1>
+                    <h2 className="text-4xl md:text-6xl font-extrabold text-black ">Sakhawat H</h2>
+                    <p className="md:text-xl font-extrabold text-[#909090]">Front-end Developer / React Next.js</p>
                 </div>
 
-                <div className='flex space-x-6 '>
+                <div className='flex space-x-6 justify-center'>
                     <Link href="https://github.com/Sakhawat71" target="_blank">
                         <FaTwitter className='text-2xl w-9 h-9' />
                     </Link>
