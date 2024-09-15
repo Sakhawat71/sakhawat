688f457f12c07f6f4df131be2685db634b55d416
sakhawat71 <sakhawat.showrav@gmail.com>
2024-09-16 01:22:41 +0600

    footer complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,37 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
-import { AiOutlineFacebook, AiOutlineLinkedin, AiOutlineMail } from "react-icons/ai";
-import { LiaGithubSquare } from "react-icons/lia";
+import ScrollToTop from 'react-scroll-to-top';
+import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { GoMail } from "react-icons/go";
 
 const Footer = () => {
 
 
     return (
-        <div className='bg-[#1A1A1A] text-white text-center my-60'>
+        <div className='bg-[#1A1A1A] text-white text-center flex flex-col items-center justify-center py-16'>
 
-            <div className='flex '>
-                <Link href={"/"}> <AiOutlineFacebook />  </Link>
-                <Link href={"/"}> <AiOutlineLinkedin />  </Link>
-                <Link href={"/"}> <LiaGithubSquare />  </Link>
-                <Link href={"/"}> <AiOutlineMail />  </Link>
+            {/* <Link href={"/"} >BACK TO TOP</Link> */}
+            <button >Download CV </button>
+
+            <ScrollToTop
+                smooth
+                className='pl-1'
+            />
+
+
+            <div className='flex py-5 w-full items-center justify-center space-x-5'>
+
+                <Link href={"/"} > <FaFacebook className='w-8 h-8 ' />  </Link>
+
+                <Link href={"/"}> <FaLinkedin className='w-8 h-8 ' />  </Link>
+
+                <Link href={"/"}> <FaGithub className='w-8 h-8 ' />  </Link>
+
+                <Link href={"/"}> <GoMail className='w-8 h-8 ' />  </Link>
             </div>
 
-            <small>@2020 Tomasz Gajda All Rights Reserved.</small>
+            <small>@2020 Sakhawat All Rights Reserved.</small>
         </div>
     );
 };
