0c08bd64b072a728902b0dfe6a4a32854cb5f48f
sakhawat71 <sakhawat.showrav@gmail.com>
2024-09-17 15:36:47 +0600

    font and skills grid

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,10 +4,41 @@ import "./skills.css";
 
 const Skills = () => {
     return (
-        <>
-            <div className='test-bg  h-[2000px]'>
+        <div className='gradient-bg h-[2000px] mt-10 mx-auto w-3/4 font-montserrat'>
+
+            <div className=' border-black border-[5px] w-60 mx-auto text-center'>
+                <h2 className='font-bold text-xl tracking-[6px] p-3'>SKILLS</h2>
             </div>
-        </>
+
+            <h1>Use in </h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  text-center border border-black'>
+                <h2>hello</h2>
+                <h2>hi</h2>
+                <h2>our</h2>
+                <h2>my</h2>
+                <h2>you</h2>
+                <h2>you</h2>
+                <h2>you</h2>
+                <h2>you</h2>
+                <h2>you</h2>
+            </div>
+
+
+            <h1>Use in </h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  text-center border border-black'>
+                <h2>hello</h2>
+                <h2>hi</h2>
+                <h2>our</h2>
+                <h2>my</h2>
+                <h2>you</h2>
+                <h2>you</h2>
+                <h2>you</h2>
+                <h2>you</h2>
+                <h2>you</h2>
+            </div>
+
+
+        </div>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,14 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'custom-radial': 'radial-gradient(50% 50% at 50% 50%, rgba(26, 21, 21, 0.00) 0%, rgba(1, 1, 1, 0.16) 100%), #FFF',
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        raleway: ["Raleway", "sans-serif"]
+        raleway: ["Raleway", "sans-serif"],
+        montserrat : ["Montserrat", "sans-serif"],
+        
       }
     },
   },
