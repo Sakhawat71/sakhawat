742d56db09b3caef54e2187e97f0d56b9ae6daae
sakhawat71 <sakhawat.showrav@gmail.com>
2024-09-17 19:05:24 +0600

    skills -> fornt end -> backend -> tools

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import "./skills.css";
+import Image from 'next/image';
+import html5 from "@/assets/html.png";
+import css3 from "@/assets/css3.png";
+import es6 from "@/assets/es6.png";
+import reactjs from "@/assets/reactjs.png";
+import nextjs from "@/assets/nextjs.png";
+import tailwind from "@/assets/tailwind.png";
+import mui from "@/assets/mui.png";
+import nodejs from "@/assets/nodejs.png";
+import express from "@/assets/expressjs.png";
+import mongodb from "@/assets/mongodb.png";
+import git from "@/assets/git.png";
+import vscode from "@/assets/vs-code.png";
+import figma from "@/assets/figma.png";
 
 
 const Skills = () => {
@@ -10,32 +24,209 @@ const Skills = () => {
                 <h2 className='font-bold text-xl tracking-[6px] p-3'>SKILLS</h2>
             </div>
 
-            <h1>Use in </h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  text-center border border-black'>
-                <h2>hello</h2>
-                <h2>hi</h2>
-                <h2>our</h2>
-                <h2>my</h2>
-                <h2>you</h2>
-                <h2>you</h2>
-                <h2>you</h2>
-                <h2>you</h2>
-                <h2>you</h2>
+            <h1 className='font-bold text-xl tracking-widest mt-20'>Front-End</h1>
+            
+            {/* front end section */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  text-center  gap-10 my-10'>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={html5}
+                        alt='html5'
+                        width={80}
+                        height={80}
+                    />
+                    <p>HTML5</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={css3}
+                        alt='html5'
+                        width={80}
+                        height={80}
+                    />
+                    <p>CSS3</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={es6}
+                        alt='html5'
+                        width={80}
+                        height={80}
+                    />
+                    <p>JS</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={reactjs}
+                        alt='reactjs'
+                        width={80}
+                        height={80}
+                    />
+                    <p>REACT.JS</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={nextjs}
+                        alt='nextjs'
+                        width={80}
+                        height={80}
+                    />
+                    <p>NEXT.JS</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={tailwind}
+                        alt='tailwind'
+                        width={80}
+                        height={80}
+                    />
+                    <p>TAILWIND CSS</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={mui}
+                        alt='mui'
+                        width={80}
+                        height={80}
+                    />
+                    <p>MATERIAL UI</p>
+                </div>
+
+
+
             </div>
 
 
-            <h1>Use in </h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  text-center border border-black'>
-                <h2>hello</h2>
-                <h2>hi</h2>
-                <h2>our</h2>
-                <h2>my</h2>
-                <h2>you</h2>
-                <h2>you</h2>
-                <h2>you</h2>
-                <h2>you</h2>
-                <h2>you</h2>
+            <h1 className='font-bold text-xl tracking-widest mt-20'>Back End</h1>
+
+            {/* back end section */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  text-center  gap-10 my-10'>
+                
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={nodejs}
+                        alt='nodejs'
+                        width={80}
+                        height={80}
+                    />
+                    <p>NODE.JS</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={express}
+                        alt='expressjs'
+                        width={150}
+                        height={80}
+                    />
+                    <p>EXPRESS.JS</p>
+                </div>
             </div>
+
+
+
+            <h1 className='font-bold text-xl tracking-widest mt-20'>Databases</h1>
+
+            {/* Databases section */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  text-center  gap-10 my-10'>
+                
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={mongodb}
+                        alt='MongoDB'
+                        width={80}
+                        height={80}
+                    />
+                    <p>MongoDB</p>
+                </div>
+
+                
+            </div>
+
+
+            <h1 className='font-bold text-xl tracking-widest mt-20'>Tools</h1>
+
+            {/* Tools section */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  text-center  gap-10 my-10'>
+                
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={git}
+                        alt='github'
+                        width={80}
+                        height={80}
+                    />
+                    <p>GitHub</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={vscode}
+                        alt='vscode'
+                        width={80}
+                        height={80}
+                    />
+                    <p>VS CODE</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={figma}
+                        alt='figma'
+                        width={80}
+                        height={80}
+                    />
+                    <p>FIGMA</p>
+                </div>
+                
+            </div>
+
+            
+            <h1 className='font-bold text-xl tracking-widest mt-20'>Tools</h1>
+
+            {/* Tools section */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  text-center  gap-10 my-10'>
+                
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={git}
+                        alt='github'
+                        width={80}
+                        height={80}
+                    />
+                    <p>GitHub</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={vscode}
+                        alt='vscode'
+                        width={80}
+                        height={80}
+                    />
+                    <p>VS CODE</p>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center space-y-3'>
+                    <Image
+                        src={figma}
+                        alt='figma'
+                        width={80}
+                        height={80}
+                    />
+                    <p>FIGMA</p>
+                </div>
+                
+            </div>
+
+             
 
 
         </div>
