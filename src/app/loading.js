60981daa2c8ba937388deb8f8998069200fc3a8c
sakhawat71 <sakhawat.showrav@gmail.com>
2024-10-05 13:02:28 +0600

    loading page design

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,9 +1,21 @@
+"use client"
 import React from 'react';
+import { RotatingLines } from 'react-loader-spinner';
 
 const LoadingPage = () => {
     return (
-        <div>
-            loading.............
+        <div className='flex justify-center items-center h-screen '>
+            <RotatingLines
+              visible={true}
+              height="96"
+              width="96"
+              color="grey"
+              strokeWidth="5"
+              animationDuration="0.75"
+              ariaLabel="rotating-lines-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
         </div>
     );
 };
