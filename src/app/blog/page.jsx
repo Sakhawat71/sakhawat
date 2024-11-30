c5d9d7e05474fe9ba83a40dc7877abf4e4f286a9
sakhawat71 <sakhawat.showrav@gmail.com>
2024-11-30 23:57:10 +0600

    under_construction  image added in blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
 import React from 'react';
+import Construction from '@/assets/ext/under_construction.png';
 
 const Blog = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-5">
+            <Image
+                src={Construction}
+                alt='Construction'
+                height={400}
+                width={400}
+                className='my-5 rounded-3xl'
+            />
             <h1 className="text-4xl font-bold">My Blog</h1>
             <p className="mt-4 text-lg text-gray-700 max-w-2xl text-center">
                 Welcome to my blog! Here, I share my thoughts on web development, tutorials, and the latest trends in technology.
